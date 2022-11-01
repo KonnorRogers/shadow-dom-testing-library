db3bb0c57f9519a7bf5a608eeee23fd15b64d4f8
Konnor Rogers <konnor5456@gmail.com>
2022-11-01 15:24:34 -0400

    feat: add shadow dom serialization support

--- __tests__/screenDebug.test.tsx
+++ __tests__/screenDebug.test.tsx
@@ -1,61 +1,62 @@
 import React from "react"
 import { render } from "@testing-library/react";
-import { logDOM } from "../src/scratch"
-
-// getDocument
-import { screen } from "../src/index";
 import { Duplicates } from "../components";
+import { screen } from "../src/index";
 
-// export function prettyPrintDOM(root: Element = document.body): string {
-// 	let htmlString = root.outerHTML;
-// 	const nodesToProcess = [...root.children];
-// 	while (nodesToProcess.length > 0) {
-// 		const node = nodesToProcess.shift()!;
-// 		if (node.shadowRoot !== null) {
-// 			const tempNode = document.createElement("div");
-// 			tempNode.innerHTML = node.outerHTML;
-// 			const shadowRootPseudoNode = document.createElement("shadow-root");
-// 			shadowRootPseudoNode.innerHTML = node.shadowRoot.innerHTML;
-// 			tempNode.firstElementChild!.insertBefore(shadowRootPseudoNode, tempNode.firstElementChild!.firstChild);
-// 			htmlString = htmlString.replace(node.outerHTML, tempNode.innerHTML);
-// 		}
-// 		nodesToProcess.push(...node.children);
-// 	}
-// 	// Remove Comment nodes
-// 	htmlString = htmlString.replace(/<!--.*?-->/g, "");
-// 	const dom = new JSDOM(htmlString);
-// 	const copiedDocument = dom.window.document;
-// 	// Remove large, invisible elements which aren't useful for debugging.
-// 	copiedDocument.querySelectorAll("script, style").forEach((elem) => {
-// 		elem.remove();
-// 	});
-// 	const DOMString = prettier.format(copiedDocument.body.innerHTML)
-// 	return DOMString
-// }
+// @TODO: this is supposed to work but doesnt...
+test.skip("Should error if two similar nodes are in shadow root", async () => {
+	render(
+		<Duplicates>
+			<div slot="start">Start Slot</div>
+			<div>Default Slot</div>
+			<div slot="end">End Slot</div>
+		</Duplicates>
+	);
 
-screen.debug = function (...args: Parameters<typeof screen.debug>) {
-  let [element, maxLength, options] = args
-
-	Array.isArray(element)
-    ? element.forEach(el => logDOM(el, maxLength, options))
-    : logDOM(element, maxLength, options);
-}
-
-
-// @see https://github.com/KonnorRogers/shadow-dom-testing-library/issues/10
-test("Should only return 1 node instead of checking both", async () => {
-  render(<h1>Hello</h1>);
-  // https://github.com/KonnorRogers/shadow-dom-testing-library/issues/10
-  expect(await screen.findByShadowText("Hello")).toBeInTheDocument();
-});
-  
-
-test("Should error if two similar nodes are in shadow root", async () => {
-	// render(<Duplicates />)
-  render(
-  	<Duplicates />
-  );
-
+	jest.spyOn(console, 'log').mockImplementation(() => { })
 	screen.debug()
+
+	// https://github.com/testing-library/dom-testing-library/blob/edffb7c5ec2e4afd7f6bedf842c669ddbfb73297/src/__tests__/pretty-dom.js#L61
+  expect(console.log).toHaveBeenCalledTimes(1)
+  // @ts-expect-error
+	expect(console.log.mock.calls[0][0]).toEqual(`
+<body>
+	<div>
+  	<duplicate-buttons>
+    	<shadow-🥷-root>
+      	<slot
+        	name="start"
+      	/>
+      	<button>
+        	Button One
+      	</button>
+      	<br />
+      	<slot />
+      	<br />
+      	<button>
+        	Button Two
+      	</button>
+      	<slot
+        	name="end"
+      	/>
+    	</shadow-🥷-root>
+    	<div
+      	slot="start"
+    	>
+      	Start Slot
+    	</div>
+    	<div>
+      	Default Slot
+    	</div>
+    	<div
+      	slot="end"
+    	>
+      	End Slot
+    	</div>
+  	</duplicate-buttons>
+	</div>
+</body>
+`)
+	jest.resetAllMocks()
 })
 
