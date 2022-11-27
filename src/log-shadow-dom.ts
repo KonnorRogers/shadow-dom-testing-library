c361896b12a9f573278b6dec564a07daa509a0e0
Konnor Rogers <konnor5456@gmail.com>
2022-11-27 13:02:01 -0500

    formatting

--- src/log-shadow-dom.ts
+++ src/log-shadow-dom.ts
@@ -1,10 +1,9 @@
 import { logDOM } from "@testing-library/dom";
-import { toJSDOM } from "./pretty-shadow-dom";
 
 export function logShadowDOM(
   ...args: Parameters<typeof logDOM>
 ): ReturnType<typeof logDOM> {
   const [dom, maxLength, options] = args;
 
-  logDOM(toJSDOM(dom), maxLength, options);
+  logDOM(dom, maxLength, options);
 }
