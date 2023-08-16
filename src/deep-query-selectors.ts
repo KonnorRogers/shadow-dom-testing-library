8a28ea31f886ea2f451301e810bcef4e39503b21
Konnor Rogers <konnor5456@gmail.com>
2023-08-16 00:00:42 -0400

    migrate to sdw

--- src/deep-query-selectors.ts
+++ src/deep-query-selectors.ts
@@ -1,3 +1,4 @@
+import { findAllElements } from "shadow-dom-walk";
 import { patchWrap } from "./trick-dom-testing-library";
 import { Container, ShadowOptions } from "./types";
 
@@ -48,65 +49,12 @@ export function getAllElementsAndShadowRoots(
   container: Container,
   options: ShadowOptions = { shallow: false }
 ) {
-  const selector = "*";
-  return recurse(container, selector, options);
-}
+  // const selector = "*";
+  // return recurse(container, selector, options);
 
-function recurse(
-  container: Container,
-  selector: string,
-  options: ShadowOptions = { shallow: false },
-  elementsToProcess: (Element | ShadowRoot | Document)[] = [],
-  elements: (Element | ShadowRoot | Document)[] = []
-) {
-  // if "document" is passed in, it will also pick up "<html>" causing the query to run twice.
-  if (container instanceof Document) {
-    container = document.documentElement;
-  }
+  let maxDepth = Infinity
 
-  // I haven't figured this one out, but for some reason when using the buildQueries
-  // from DOM-testing-library, not reassigning here causes an infinite loop.
-  // I've even tried calling "elementsToProcess.includes / .find" with no luck.
-  elementsToProcess = [container];
-  elements.push(container); // Make sure we're checking the container element!
+  if (options.shallow === true) { maxDepth = 1 }
 
-  // Accounts for if the container houses a textNode
-  if (
-    container instanceof HTMLElement &&
-    container.shadowRoot != null &&
-    container.shadowRoot.mode !== "closed"
-  ) {
-    elements.push(container.shadowRoot);
-    elementsToProcess.push(container.shadowRoot);
-  }
-
-  elementsToProcess.forEach((containerElement) => {
-    containerElement
-      .querySelectorAll(selector)
-      .forEach((el: Element | HTMLElement) => {
-        if (el.shadowRoot == null || el.shadowRoot.mode === "closed") {
-          elements.push(el);
-          return;
-        }
-
-        // This is here because queryByRole() requires the parent element which in some cases is the shadow root.
-        elements.push(el.shadowRoot);
-
-        if (options.shallow === true) {
-          el.shadowRoot.querySelectorAll(selector).forEach((el) => {
-            elements.push(el);
-          });
-          return;
-        }
-
-        el.shadowRoot.querySelectorAll(selector).forEach((el) => {
-          elements.push(el);
-          elementsToProcess.push(el);
-        });
-        recurse(el.shadowRoot, selector, options, elementsToProcess, elements);
-      });
-  });
-
-  // We can sometimes hit duplicate nodes this way, lets stop that.
-  return [...new Set(elements)];
+  return findAllElements(container, maxDepth)
 }
