49f82324b3f960a932fe094428996e3c0a626bff
konnorrogers <konnor5456@gmail.com>
2024-03-28 17:38:53 -0400

    prettier

--- __tests__/custom-data-attribute.test.tsx
+++ __tests__/custom-data-attribute.test.tsx
@@ -1,17 +1,16 @@
 import * as React from "react";
 import { configure, render } from "@testing-library/react";
 // import { configure } from "@testing-library/dom";
-import {screen} from "../src/index"
+import { screen } from "../src/index";
 import { NestedShadowRoots } from "../components";
 
 configure({
-  testIdAttribute: "data-cy"
-})
+  testIdAttribute: "data-cy",
+});
 
 // const Component = () => <div><template shadowrootmode="open"><div data-cy="my-component"></div></template></div>
 
-
 it("Should support custom data attributes for test id", async () => {
-  render(<NestedShadowRoots />)
-  expect(screen.getByShadowTestId("duplicate-buttons")).toBeInTheDocument()
-})
+  render(<NestedShadowRoots />);
+  expect(screen.getByShadowTestId("duplicate-buttons")).toBeInTheDocument();
+});
